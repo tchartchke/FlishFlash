c7ec9095f52e41c8da826b5a2d63cffa323ce217
tchartchke <chartai.kl@gmail.com>
2021-01-12 23:46:04 -0600

    Fixed signin and started collections

--- app/controllers/collection_controller.rb
+++ app/controllers/collection_controller.rb
@@ -1,5 +1,18 @@
 class CollectionsController < ApplicationController
-  get '/collections/new' do
-    erb :'collections/new'
+  post '/collections' do
+    if signed_in?
+      @collection = Collection.new(params[:collection])
+      @collection.user = current_user
+      @collection.save
+      redirect 'collections/:slug'
+    else
+      redirect '/signin'
+    end
+    
+  end
+
+  get '/collections/:slug' do
+    @collection = Collection.find_by_slug(params[:slug])
+    erb :'collections/show'
   end
 end
