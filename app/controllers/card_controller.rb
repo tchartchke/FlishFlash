199154d2faf6fcbc438d06d8e9f9ce6b044c3e14
tchartchke <chartai.kl@gmail.com>
2021-01-13 13:39:01 -0600

    Card added notification

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -8,6 +8,24 @@ class CardsController < ApplicationController
     erb :'cards/new'
   end
 
+  post '/collections/:id/new_card' do
+    @msg = nil
+    @collection = Collection.find(params[:id])
+    if signed_in? && @collection.user == current_user
+      card = Card.create(params[:card])
+      card.collection = @collection
+      if card.save
+        @msg = "New Card added to Collection!"
+      else
+        @msg = "Error: Unable to add new card"
+      end
+      erb :'cards/new'
+    else
+      redirect '/signin'
+    end
+  end
+  
+
   get '/card/:id' do
     @card = Card.find(params[:id])
     erb :'cards/show', :layout => :card
